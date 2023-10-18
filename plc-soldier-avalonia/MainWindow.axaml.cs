31e84380b53c5a30e2ba8132ce4b02a4a89cff9e
Disorol <49810312+Disorol@users.noreply.github.com>
2023-10-18 17:59:59 +0500

    Added some styles. Added some TabItems. Added the ability to add TabItems to the TabControl at startup. Some comments have been added and changed.

--- plc-soldier-avalonia/MainWindow.axaml.cs
+++ plc-soldier-avalonia/MainWindow.axaml.cs
@@ -10,14 +10,16 @@ namespace plc_soldier_avalonia
         // List of content for bottom space TabItems
         List<BottomTabItem> bottomItems = new List<BottomTabItem>()
         {
-            new BottomTabItem(){Content = "какой то текст", Title = "Консоль" },
-
+            new BottomTabItem(){Content = "какой-то текст", Title = "Ошибки" },
+            new BottomTabItem(){Content = "какой-то текст", Title = "Поиск результатов" },
+            new BottomTabItem(){Content = "какой-то текст", Title = "Просмотр" },
         };
 
         // List of content for left space TabItems
         List<LeftTabItem> leftItems = new List<LeftTabItem>()
         {
-            new LeftTabItem(){Content = "какой то текст", Title = "Обозреватель решений" },
+            new LeftTabItem(){Content = "какой-то текст", Title = "Логический органайзер" },
+            new LeftTabItem(){Content = "какой-то текст", Title = "Контроллер-органайзер" },
         };
 
         // A list containing bottom space Tabitems
@@ -30,10 +32,27 @@ namespace plc_soldier_avalonia
             BottomContent = new ObservableCollection<BottomTabItem>();
             LeftContent = new ObservableCollection<LeftTabItem>();
 
+            AddingTabItemsAtStartup(new List<LeftTabItem>() { leftItems[0] }, 
+                                    new List<BottomTabItem>() { bottomItems[0] });
+
             InitializeComponent();
 
             BottomSpace.ItemsSource = BottomContent;
             LeftSpace.ItemsSource = LeftContent;
+        }
+
+        // Adding TabItems to TabControl at the startup
+        private void AddingTabItemsAtStartup(List<LeftTabItem> leftItemsStartup, List<BottomTabItem> bottomItemsStartup)
+        {
+            foreach (var leftItem in leftItemsStartup)
+            { 
+                LeftContent.Add(leftItem);
+            }
+
+            foreach (var bottomItem in bottomItemsStartup)
+            {
+                BottomContent.Add(bottomItem);
+            }
         }
 
         // Removing TabItem
@@ -52,16 +71,34 @@ namespace plc_soldier_avalonia
             }
         }
 
-        // Creating the Solution Explorer TabItem
-        private void Solution_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        // Creating the Logical Organizer TabItem
+        private void LogicalOrganizer_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
             if (!LeftContent.Contains(leftItems[0])) LeftContent.Add(leftItems[0]);
         }
 
-        // Creating the Console TabItem
-        private void Console_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        // Creating the Control Organizer TabItem
+        private void ControllerOrganizer_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (!LeftContent.Contains(leftItems[1])) LeftContent.Add(leftItems[1]);
+        }
+
+        // Creating the Errors TabItem
+        private void Errors_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
             if (!BottomContent.Contains(bottomItems[0])) BottomContent.Add(bottomItems[0]);
+        }
+
+        // Creating the SearchResults TabItem
+        private void SearchResults_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (!BottomContent.Contains(bottomItems[1])) BottomContent.Add(bottomItems[1]);
+        }
+
+        // Creating the Watch TabItem
+        private void Watch_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (!BottomContent.Contains(bottomItems[2])) BottomContent.Add(bottomItems[2]);
         }
 
         private void Exit_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
