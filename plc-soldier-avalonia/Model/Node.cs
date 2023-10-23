527b14c1329f39081efeaff90943c8c6626b20c3
Disorol <49810312+Disorol@users.noreply.github.com>
2023-10-23 12:06:51 +0500

    Added a model with file hierarchy logic.

---
+++ plc-soldier-avalonia/Model/Node.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plc_soldier_avalonia.Model
+{
+    /*
+        A class with a recursive overloaded constructor for traversing through all directories
+        and creating a hierarchy of directories and files using ObservableCollection.
+        !!! Files and directories are considered different concepts !!!
+    */
+    public class Node
+    {
+        // Collection of child files for this directory. Can be Null for storing files.
+        public ObservableCollection<Node>? Subfiles { get; set; }
+
+        // File or directory title. Maybe Null so that empty folders can be opened.
+        public string? FileName { get; set; }
+
+        // The path to this file. Maybe Null so that empty folders can be opened.
+        public string? PathString { get; set; }
+
+        // Overloaded Constructor for the directory path.
+        public Node(string path)
+        {
+            PathString = path;
+
+            FileName = System.IO.Path.GetFileName(path);
+
+            Subfiles = new ObservableCollection<Node>();
+
+            if (Directory.GetFileSystemEntries(path, "*", SearchOption.TopDirectoryOnly).Length > 0)
+            {
+                if (Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly).Length > 0)
+                {
+                    foreach (string subpath in Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly))
+                    {
+                        Node node = new Node(subpath);
+
+                        Subfiles.Add(node);
+                    }
+                }
+
+                if (Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly).Length > 0)
+                {
+                    foreach (string subpath in Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly))
+                    {
+                        Node node = new Node(subpath, true);
+
+                        Subfiles.Add(node);
+                    }
+                }
+            }
+            else
+            {
+                // Creating an empty directory
+
+                Node node = new Node(true);
+
+                Subfiles.Add(node);
+            }
+        }
+
+        // Overloaded Constructor for the file path.
+        public Node(string path, bool isFile)
+        {
+            PathString = path;
+
+            FileName = System.IO.Path.GetFileName(path);
+        }
+
+        // Overloaded constructor for opening empty directories
+        public Node(bool isEmpty) {}
+    }
+}
